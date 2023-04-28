a0e41bdf8b656cc97021fc6c35e90e6a21923237
raul_zannoni <raul.zannnoni92@gmail.com>
2023-04-28 14:57:06 +0200

    features

--- index.php
+++ index.php
@@ -48,6 +48,28 @@
                     <img src="illustration.svg" alt="image_ordinateur_clique" id="home_cat">
                 </div>
             </main>   
+        </section>
+        <section id="features">
+            <h2>Products Features</h2>
+            <p>It is a long established fact that a reader will be of a page when established fact looking at its layout</p>
+            <div id="features_container">
+                <article class="features_products">
+                    <i class="fa-solid fa-globe"></i>
+                    <h3>Digital Design</h3>
+                    <p>Some quick example test to build on the card title and make up the bulk of the card the platform.</p>
+                </article>
+                <article class="features_products">
+                    <i class="fa-solid fa-brush"></i>
+                    <h3>Unlimited Colors</h3>
+                    <p>Credibily brand stanoods compliant user exteible services College Anibh Ocean euismod tincudunt laareet.</p>
+                </article>
+                <article class="features_products">
+                    <i class="fa-solid fa-chess"></i>
+                    <h3>Strategy Solutions</h3>
+                    <p>Separated they live in Bookmarks grove right at the coast of the Semantics, a large ocean regelialia.</p>
+                </article>
+                
+            </div>
         </section>    
     </body>
 </html>
