1a0d8917ca24b9b117eb7fe87a13322c5e22e644
raul_zannoni <raul.zannnoni92@gmail.com>
2023-04-28 16:17:37 +0200

    links correction

--- index.php
+++ index.php
@@ -16,21 +16,21 @@
                 </div>
                 <div id="header_search">
                     <ul>
-                        <li><a class="nav_text">HOME</a></li>
-                        <li><a class="nav_text">FEATURES</a></li>
-                        <li><a class="nav_text">CLIENT</a></li>
-                        <li><a class="nav_text">PRICING</a></li>
-                        <li><a class="nav_text">FAQ</a></li>
-                        <li><a class="nav_text">ABOUT</a></li>
-                        <li><a class="nav_text">BLOG</a></li>
-                        <li><a class="nav_text">CONTACT</a></li>
+                        <li><a href="" class="nav_text">HOME</a></li>
+                        <li><a href="" class="nav_text">FEATURES</a></li>
+                        <li><a href="" class="nav_text">CLIENT</a></li>
+                        <li><a href="" class="nav_text">PRICING</a></li>
+                        <li><a href="" class="nav_text">FAQ</a></li>
+                        <li><a href="" class="nav_text">ABOUT</a></li>
+                        <li><a href="" class="nav_text">BLOG</a></li>
+                        <li><a href="" class="nav_text">CONTACT</a></li>
                     <ul>
                 </div>
                 <div id="header_social">
                         <ul>
-                            <li><a class="social_logo"><i class="fa-brands fa-facebook-f"></i></a></li>
-                            <li><a class="social_logo"><i class="fa-brands fa-twitter"></i></a></li>
-                            <li><a class="social_logo"><i class="fa-brands fa-instagram"></i></a></li>
+                            <li><a href="" class="social_logo"><i class="fa-brands fa-facebook-f"></i></a></li>
+                            <li><a href="" class="social_logo"><i class="fa-brands fa-twitter"></i></a></li>
+                            <li><a href="" class="social_logo"><i class="fa-brands fa-instagram"></i></a></li>
                         </ul>
                     </div>
             </header>
@@ -44,7 +44,6 @@
                             <input type="submit" name="submit" value="SUBSCRIBE">
                         </form>
                     </div>
-
                     <img src="illustration.svg" alt="image_ordinateur_clique" id="home_cat">
                 </div>
             </main>   
@@ -69,37 +68,31 @@
                     <p>Separated they live in Bookmarks grove right at the coast of the Semantics, a large ocean regelialia.</p>
                 </article>
             </div>
+            <h2>How It Works ?</h2>
+            <p>It is a long established fact that a reader will be of a page established fact looking at its layout.</p>
         </section>
         <section id="team">
-
         <h2>We'r dynamic team of talented people of innovative & marketing expert</h2>
         <p>To achieve this, it would be necessary to have uniform grammar, pronunciation and more commun words.
             If several languages of the resulting language.</p>
-
         <div id="team_photos">
-
             <article>
                 <h4>1499+</h4>
                 <p>Complete Projects</p>
             </article>
-
             <article>
                 <h4>1080K</h4>
                 <p>Satisfied Clients</p>
             </article>
-
             <article>
                 <h4>608</h4>
                 <p>Team Members</p>
             </article>
-
             <article>
                 <h4>252</h4>
                 <p>Employees</p>
             </article>
-
         </div>
-
         <div id="portraits">
             <div class="team_card">
                 <div class="team_card_header">
